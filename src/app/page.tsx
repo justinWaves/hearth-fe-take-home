0e38338e4a163f89cdb29f1cc2314ab7102609df
Justin Weisberg <juhstinn@gmail.com>
2024-05-06 13:12:45 -0700

    fix cicd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import Button from "@/elements/button/Button";
+import Button from "@/elements/Button/Button";
 import "./homepage.scss";
 import { bemElement } from "../utils/bem-class-names";
 
